0414ff504af1ed775c2bebab14f51bc30c90413e
Aleksei Anisimov <suxelshock@gmail.com>
2024-04-02 20:42:28 +0300

    Работа с ресурсами и верстка

--- app/src/main/java/ru/yandex/practicum/sprint8koh16/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh16/MainActivity.kt
@@ -1,11 +1,12 @@
 package ru.yandex.practicum.sprint8koh16
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val isTablet = resources.getBoolean(R.bool.is_tablet)
     }
 }
